2f196a7a39dd4a1a0ea266f80dad4357f0fc1078
Valentin Dealbera <valentindealbera01@gmail.com>
2023-05-11 20:12:15 -0300

    Home done no animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent,
     MainComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,

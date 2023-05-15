63fc3707d6aeae161fbd083811bb2acc9f449224
Valentin Dealbera <valentindealbera01@gmail.com>
2023-05-15 02:16:14 -0300

    navigation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Valentin Dealbera';
+  @ViewChild('home') home!: ElementRef;
+  @ViewChild('about') about!: ElementRef;
+  @ViewChild('skills') skills!: ElementRef;
+  @ViewChild('projects') projects!: ElementRef;
+  @ViewChild('contact') contact!: ElementRef;
+  scrollToHome() {
+    this.home.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+  scrollToAbout() {
+    const headerOffset = 45;
+    const elementPosition = this.about.nativeElement.getBoundingClientRect().top
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    window.scrollTo({ behavior: 'smooth', top: offsetPosition});
+  }
+  scrollToSkills() {
+    const headerOffset = 45;
+    const elementPosition = this.skills.nativeElement.getBoundingClientRect().top
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    window.scrollTo({ behavior: 'smooth', top: offsetPosition});
+  }
+  scrollToProjects() {
+    const headerOffset = 45;
+    const elementPosition = this.projects.nativeElement.getBoundingClientRect().top
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    window.scrollTo({ behavior: 'smooth', top: offsetPosition});
+  }
+  scrollToContact() {
+    const headerOffset = 45;
+    const elementPosition = this.contact.nativeElement.getBoundingClientRect().top
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    window.scrollTo({ behavior: 'smooth', top: offsetPosition});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { MainComponent } from './components/main/main.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
@@ -15,8 +13,6 @@ import { ContactComponent } from './components/contact/contact.component'
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    MainComponent,
     HomeComponent,
     AboutComponent,
     SkillsComponent,

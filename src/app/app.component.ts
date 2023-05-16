2eb1ed19970de7bff1b1fc51ad3fa72054429c8c
Valentin Dealbera <valentindealbera01@gmail.com>
2023-05-16 13:48:41 -0300

    navBar Responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,29 +14,38 @@ export class AppComponent {
   @ViewChild('contact') contact!: ElementRef;
   scrollToHome() {
     this.home.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    this.dropdown = false
   }
   scrollToAbout() {
     const headerOffset = 45;
     const elementPosition = this.about.nativeElement.getBoundingClientRect().top
     const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
     window.scrollTo({ behavior: 'smooth', top: offsetPosition});
+    this.dropdown = false
   }
   scrollToSkills() {
     const headerOffset = 45;
     const elementPosition = this.skills.nativeElement.getBoundingClientRect().top
     const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
     window.scrollTo({ behavior: 'smooth', top: offsetPosition});
+    this.dropdown = false
   }
   scrollToProjects() {
     const headerOffset = 45;
     const elementPosition = this.projects.nativeElement.getBoundingClientRect().top
     const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
     window.scrollTo({ behavior: 'smooth', top: offsetPosition});
+    this.dropdown = false
   }
   scrollToContact() {
     const headerOffset = 45;
     const elementPosition = this.contact.nativeElement.getBoundingClientRect().top
     const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
     window.scrollTo({ behavior: 'smooth', top: offsetPosition});
+    this.dropdown = false
+  }
+  dropdown: Boolean = false
+  setDropDown():void {
+    this.dropdown = !this.dropdown
   }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  @Output() MyWorks = new EventEmitter<void>();
+  @Output() HireMe = new EventEmitter<void>();
 
+  triggerMyWorks() {
+    this.MyWorks.emit();
+  }
+  triggerHireMe() {
+    this.HireMe.emit();
+  }
 }
